21308543f28404430c925ca364e1fcf4507b0611
sharapov2701 <sharapov2701@gmail.com>
2021-03-09 22:42:34 +0300

    добавить роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route } from "react-router-dom"
 import History from './views/History'
+import Income from './views/Income'
+import Expenses from './views/Expenses'
 
 const App = () => {
   return (
-    <History />
+    <BrowserRouter>
+      <Route path='/' component={History} exact />
+      <Route path='/income' component={Income} exact />
+      <Route path='/expenses' component={Expenses} exact />
+    </BrowserRouter>
   )
 }
 
